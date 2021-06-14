4962f4963c286387b00ed42e60865aef4589cd32
Dillon Travers <dillon@Rori.local>
2021-06-14 15:13:29 -0400

    d6

--- keyboards/deedee6/config.h
+++ keyboards/deedee6/config.h
@@ -28,10 +28,10 @@ GNU General Public License <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 7
 
-// wiring
-#define MATRIX_ROW_PINS { B4, E6, D7, D4 }
-#define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5 }
-#define UNUSED_PINS
+// wiring (each half)
+#define MATRIX_ROW_PINS { D2, D3, D4, D5 }
+#define MATRIX_COL_PINS { F7, F6, F5, F4, F1, F0, E6 }
+//#define UNUSED_PINS
 
 
 /* COL2ROW, ROW2COL*/
@@ -47,3 +47,4 @@ GNU General Public License <http://www.gnu.org/licenses/>.
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
 #define SOFT_SERIAL_PIN D0
+#define SELECT_SOFT_SERIAL_SPEED 1
--- keyboards/deedee6/keymaps/default/keymap.c
+++ keyboards/deedee6/keymaps/default/keymap.c
@@ -16,10 +16,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT(
-    KC_W,   KC_W, KC_E,   KC_R,      KC_T, KC_Y, KC_W,        KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,   KC_W,
-    KC_ESC, KC_A, KC_S,   KC_D,      KC_F, KC_G, KC_W,        KC_W, KC_H, KC_J, KC_K, KC_L,    KC_W,   KC_ESC,
-            KC_Z, KC_W,   KC_X,      KC_C, KC_V,                    KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
-                  KC_SPC, KC_LSHIFT, KC_W,                                KC_W, KC_W, KC_SPC
+    KC_W,   KC_W, KC_E, KC_R, KC_T,   KC_Y,      KC_W,        KC_T, KC_Y, KC_U,   KC_I, KC_O,    KC_P,   KC_W,
+    KC_ESC, KC_A, KC_S, KC_D, KC_F,   KC_G,      KC_W,        KC_W, KC_H, KC_J,   KC_K, KC_L,    KC_W,   KC_ESC,
+    KC_Z,   KC_Z, KC_W, KC_X, KC_C,   KC_V,      KC_Z,        KC_Z, KC_B, KC_N,   KC_M, KC_COMM, KC_DOT, KC_Z,
+                        KC_Z, KC_SPC, KC_LSHIFT, KC_W,        KC_W, KC_W, KC_SPC, KC_Z
     ),
 };
 
