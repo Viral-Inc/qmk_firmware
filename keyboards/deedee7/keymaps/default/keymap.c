3a531d527ea17c2e8adfffc5b2a649fae04f2f9e
Dillon Travers <dillon@Rori.local>
2021-07-21 12:22:29 -0400

    x

--- keyboards/deedee7/keymaps/default/keymap.c
+++ keyboards/deedee7/keymaps/default/keymap.c
@@ -16,7 +16,8 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BASE
+    _BASE,
+    _NUMB
 };
 
 
@@ -24,10 +25,28 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
 [_BASE] = LAYOUT(
-   KC_Q,   KC_W, KC_E, KC_R, KC_T,      KC_0,                             KC_Y,   KC_U,  KC_I, KC_O,    KC_P,    KC_ESC,
-   KC_ESC, KC_A, KC_S, KC_D, KC_F,      KC_G,      KC_1,          KC_5,   KC_H,   KC_J,  KC_K, KC_L,    KC_SCLN, KC_QUOT,
-           KC_Z, KC_X, KC_X, KC_C,      KC_V,      KC_2,          KC_6,   KC_B,   KC_N,  KC_M, KC_COMM, KC_DOT,
-                             KC_LSHIFT, KC_LSHIFT, KC_LSHIFT,     KC_SPC, KC_SPC, KC_SPC
-   )
+   KC_Q,   KC_W, KC_E, KC_R, KC_T,              KC_0,                                     KC_Y,   KC_U,  KC_I,    KC_O,     KC_P,    KC_ESC,
+   KC_ESC, KC_A, KC_S, KC_D, KC_F,              KC_G,      KC_1,                  KC_5,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+           KC_Z, KC_X, KC_X, KC_C,              KC_V,      KC_2,                  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLASH,
+                             LT(_NUMB, KC_DEL), KC_LSHIFT, KC_LSHIFT,             KC_SPC, KC_SPC, KC_SPC
+   ),
+[_NUMB] = LAYOUT(
+  _______, KC_0,    KC_1, KC_2, KC_3, _______,                            KC_0,    KC_1, KC_2, KC_3, _______, _______,
+  _______, _______, KC_4, KC_5, KC_6, _______, _______,          _______, _______, KC_4, KC_5, KC_6, _______, _______,
+           _______, KC_7, KC_8, KC_9, _______, _______,          _______, _______, KC_7, KC_8, KC_9, _______,
+                             _______, _______, _______,          _______, _______, _______
+  )
 };
+
+
+
+/*
+* [_BASE] = LAYOUT(
+*   _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+*   _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+*            _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+*                                       _______, _______, _______,          _______, _______, _______
+*   )
+* };
+*/
 
